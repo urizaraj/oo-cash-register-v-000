01088e367c6fd179d2f7f6617af1105ec32bcd84
Alexander Urizar <urizaraj@gmail.com>
2018-02-12 18:54:21 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,7 +13,7 @@ class CashRegister
     quantity.times do
       self.items << title
     end
-    self.last_transaction = [title, price, quantity]
+    self.last_transaction = [price, quantity]
   end
 
   def apply_discount
